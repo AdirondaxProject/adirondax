cb1f6fbd4c3d2b6b76cc10f2fd0a510c3610196b
pmocz <philip.mocz@gmail.com>
2025-01-15 19:02:29 -0500

    update tolerances

--- tests/test_inverse_problem.py
+++ tests/test_inverse_problem.py
@@ -91,4 +91,4 @@ def test_forward_model():
 
 
 def test_solve_inverse_problem():
-    assert abs(solve_inverse_problem() - 0.019558249) < 1e-2
+    assert abs(solve_inverse_problem() - 0.019558249) <2e-2
